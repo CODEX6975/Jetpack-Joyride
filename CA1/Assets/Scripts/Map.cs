0074b9561f6a36cd076877478e003faa47762ca1
CODEX6975 <jayvardhan2004ch@gmail.com>
2023-10-11 13:03:08 +0100

    Commit
    Project linked to git repository
    Base level established
    Base scripts for player and camera movement added

---
+++ CA1/Assets/Scripts/Map.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour{
+
+    public GameObject ceiling;
+    public GameObject Floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
